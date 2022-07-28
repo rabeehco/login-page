17f7339c5d871d414814cfbe1b259ffec85af523
codewithrabeeh <real.ray48@gmail.com>
2022-07-28 09:09:22 +0530

    added: login,register ejs/ optimize views folder

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const ejsMate = require('ejs-mate')
 
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(__dirname + '/public'));
@@ -14,6 +16,14 @@ app.get('/home', (req, res) => {
     res.send('Home Sweet Home')
 })
 
-app.listen(3000, () => {
-    console.log('Serving Port 3000')
+app.get('/login', (req, res) => {
+    res.render('users/login')
+})
+
+app.get('/register', (req, res) => {
+    res.render('users/register')
+})
+
+app.listen(4000, () => {
+    console.log('Serving Port 4000')
 })
